c725f6a2d7ac2a06826b32efda4ad6d70a6b9943
WaldoTaljaard <70090665+WaldoTaljaard@users.noreply.github.com>
2020-08-23 17:02:22 +0200

    Add files via upload

--- MARSAGLIA_AND_BRAY.py
+++ MARSAGLIA_AND_BRAY.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+import statistics
 
 class marsaglia_bray:
     def __init__(self, value):
@@ -85,78 +86,20 @@ class marsaglia_bray:
         plt.legend()
         plt.show()
 
+    def getMean(self):
+        return statistics.mean(self.samples)
+
+    def getStandardDeviation(self):
+        return statistics.stdev(self.samples)
+
+
 
 test = marsaglia_bray(10000)
 test.SampleRandomVariables()
+print(test.getMean())
+print(test.getStandardDeviation())
+print(test.getStandardDeviation()**2)
 test.printPDF()
 
 
-# samples = []
-#
-# for i in range(10000):
-#
-#     prob = random.random()
-#
-#     u1 = random.uniform(0, 1)
-#     u2 = random.uniform(0, 1)
-#     u3 = random.uniform(0, 1)
-#
-#     print("Random float number is ", prob)
-#     print("Random float number is ", u1)
-#     print("Random float number is ", u2)
-#     print("Random float number is ", u3)
-#
-#
-#     def g(temp):
-#         if (abs(temp) < 1.0):
-#             return 17.49731196 * math.exp(-0.5 * pow(temp, 2)) - 4.73570326 * (3 - pow(temp, 2)) - 2.15787544 * (
-#                         1.5 - abs(temp))
-#         if (abs(temp) < 1.0 and abs(temp) < 1.5):
-#             return 17.49731196 * math.exp(-0.5 * pow(temp, 2)) - 4.73570326 * (3 - pow(temp, 2)) - 2.15787544 * (
-#                         1.5 - abs(temp))
-#         if (abs(temp) < 1.5 and abs(temp) < 3.0):
-#             return 17.49731196 * math.exp(-0.5 * pow(temp, 2)) - 4.73570326 * (3 - pow(temp, 2))
-#         if (abs(temp) < 3.0):
-#             return 0
-#
-#
-#     if (prob < 0.8638):
-#         X = 2.0 * (u1 + u2 + u3 - 1.5)
-#     elif (prob < 0.9745):
-#         X = 1.5 * (u1 + u2 - 1)
-#     elif (prob < 0.9973002039):
-#         while True:
-#             x = 6 * u1 - 3
-#             y = 0.358 * u2
-#             if (y < g(x)):
-#                 break
-#             u1 = random.uniform(0, 1)
-#             u2 = random.uniform(0, 1)
-#     elif (prob < 1):
-#         while True:
-#             v1 = random.uniform(-1, 1)
-#             v2 = random.uniform(-1, 1)
-#
-#             x = v1 * ((9 - 2 * math.log(v1 ** 2 + v2 ** 2)) / (v1 ** 2 + v2 ** 2)) ** 0.5
-#             y = v2 * ((9 - 2 * math.log(v1 ** 2 + v2 ** 2)) / (v1 ** 2 + v2 ** 2)) ** 0.5
-#
-#             if (x > 3 or y > 3):
-#                 if (x > 3):
-#                     X = x
-#                 else:
-#                     X = y
-#                 break
-#     samples.append(X)
-#
-# bins = np.linspace(-5, 5, 30)
-# histogram, bins = np.histogram(samples, bins=bins, density=True)
-#
-# bin_centers = 0.5 * (bins[1:] + bins[:-1])
-#
-# pdf = stats.norm.pdf(bin_centers)
-#
-# plt.figure(figsize=(6, 4))
-# plt.plot(bin_centers, histogram, label="Histogram of samples")
-# plt.plot(bin_centers, pdf, label="PDF")
-# plt.legend()
-# plt.show()
+
